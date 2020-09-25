2609a43b919e4aa0d252fa91f727001f6a46f4cd
kowalix-pl <kowalix007@gmail.com>
2020-09-25 19:57:32 +0100

    create users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200923104125) do
+ActiveRecord::Schema.define(version: 20200925185328) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20200923104125) do
     t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "encrypted_password"
   end
 
 end

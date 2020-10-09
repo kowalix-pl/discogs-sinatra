f283305d906aa32008c9d734caac4d8f44b549ef
kowalix-pl <kowalix007@gmail.com>
2020-10-09 20:05:53 +0100

    add validation for uniqueness of email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :albums
   validates :email, format: {with:/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
     message: "Invalid email address"}
+  validates_uniqueness_of :username, message:  "Username already exists, choose another one!"
+  validates_uniqueness_of :email, message:  "Email already exists, choose another one!"
 end
